06e5f4a700c043f8fa84d3ef0c747dc7c2756cfe
Minh Duc (Mac M1) <sun123vet@gmail.com>
2024-05-17 01:45:30 +0700

    feat(pinecone-db): add langchain pdf-loader

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,10 +1,12 @@
+import { loadS3IntoPinecone } from "@/lib/pinecone";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
-    const { fikeKey, fileName } = body;
-    return NextResponse.json({ message: "success" });
+    const { fileKey, fileName } = body;
+    const pages = await loadS3IntoPinecone(fileKey);
+    return NextResponse.json({ pages });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
