d81977af75b5c89e7fdc1ba1e55dec7ad9e2a549
Minh Duc (Mac M1) <sun123vet@gmail.com>
2024-05-17 15:33:10 +0700

    feat(pinecone-db): add function to segment pdf pages

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,10 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import {
+  Document,
+  RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
 
 let pinecone: Pinecone | null = null;
 
@@ -13,6 +17,13 @@ export const getPineconeClient = async () => {
   return pinecone;
 };
 
+type PDFPage = {
+  pageContent: string;
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
+
 export async function loadS3IntoPinecone(fileKey: string) {
   console.log(`Dowloading S3 file to system...`);
   const fileName = await downloadFromS3(fileKey);
@@ -20,6 +31,31 @@ export async function loadS3IntoPinecone(fileKey: string) {
     throw new Error("Could not download from S3");
   }
   const loader = new PDFLoader(fileName);
-  const pages = await loader.load();
+  const pages = (await loader.load()) as PDFPage[];
+  const documents = await Promise.all(pages.map(prepareDocument))
+
   return pages;
+}
+
+// export function embedDocument()
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+};
+
+async function prepareDocument(page: PDFPage) {
+  let { metadata, pageContent } = page;
+  pageContent = pageContent.replace(/\n/g, "");
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
 }
